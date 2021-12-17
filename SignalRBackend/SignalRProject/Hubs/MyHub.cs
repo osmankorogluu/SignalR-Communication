9b57f3c71ea1ea4c9b1f0d7176a11b142fca8519
Osman Köroğlu <67230145+osmankorogluu@users.noreply.github.com>
2021-12-17 23:29:57 +0300

    first commit

---
+++ SignalRBackend/SignalRProject/Hubs/MyHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRProject.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+            // abi bu proje çok küçük ikisini aynı atayım
+        }
+    }
+}
